d789c7c6933800777fd5a39d7516eb96dbdf2f44
lisz1012 <lisz1012@163.com>
2022-04-05 23:45:21 -0700

    Init

---
+++ src/main/java/com/lisz/Csrf1Application.java
@@ -0,0 +1,11 @@
+package com.lisz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csrf1Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Csrf1Application.class, args);
+	}
+}
---
+++ src/main/java/com/lisz/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.lisz.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+	@GetMapping("/hi")
+	public String hi(){
+		System.out.println("来了老弟");
+		return "hi";
+	}
+}
